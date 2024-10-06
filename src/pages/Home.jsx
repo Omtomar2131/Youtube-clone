945c1ac0700e003c26f3bfb01dcde8e3b3e1eb1a
Om Tomar <omtomar21102003@gmail.com>
2024-10-06 10:59:32 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import  { useEffect } from 'react'
 import Navbar from '../components/Navbar';
 import Sidebar from '../components/Sidebar';
-import Card from '../Components/Card';
+import Card from '../components/Card';
 import Spinner from '../components/Spinner';
 
 import {useAppDispatch,useAppSelector} from "../hooks/useApp";
